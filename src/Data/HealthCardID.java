52d466f2f683ee96997e2ec8e1ce1c22810f3829
oriolaguilar <aguilars99@gmail.com>
2019-12-17 20:42:43 +0100

    Writing ProductID & HCID code

--- src/Data/HealthCardID.java
+++ src/Data/HealthCardID.java
@@ -1,4 +1,31 @@
-package Data;
+package data;
 
-public class HealthCardID {
+final public class HealthCardID {
+
+    private final String personalID;
+
+    public HealthCardID(String code) {
+        this. personalID = code;
+    }
+
+    public String getPersonalID() {
+        return personalID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HealthCardID hcardID = (HealthCardID) o;
+        return personalID.equals(hcardID.personalID);
+    }
+    @Override
+    public int hashCode() {
+        return personalID.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "HealthCardID{" + "personal code='" + personalID + '\'' + '}';
+    }
 }
--- src/Data/ProductID.java
+++ src/Data/ProductID.java
@@ -1,4 +1,29 @@
-package Data;
+package data;
 
-public class ProductID {
+final public class ProductID {
+
+    private final String UPC;
+
+    public ProductID(String code){
+        this.UPC = code;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductID prodID = (ProductID) o;
+        return this.UPC.equals(prodID.UPC);
+    }
+    @Override
+    public int hashCode() {
+        return UPC.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "ProductID{" + "Universal Product Code ='" + UPC + '\'' + '}';
+    }
+
 }

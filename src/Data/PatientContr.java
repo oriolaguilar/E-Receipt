3a31897398f05e69bf2332c1c519f67dbb540ed3
oriolaguilar <aguilars99@gmail.com>
2019-12-23 16:47:04 +0100

    Implemetation of PatientCont

--- src/Data/PatientContr.java
+++ src/Data/PatientContr.java
@@ -1,4 +1,26 @@
 package data;
 
-public class PatientContr {
+import java.math.BigDecimal;
+
+final public class PatientContr {
+
+    private BigDecimal contribution;
+
+    public PatientContr (BigDecimal contr) throws WrongCodeException {
+
+        if (contr == null) throw new WrongCodeException();
+        this.contribution = contr;
+    }
+
+    public BigDecimal getContribution() {
+        return contribution;
+    }
+
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PatientContr patCont = (PatientContr) o;
+        return contribution.equals(patCont.contribution);
+
+    }
 }
